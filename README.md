d2ede8a39d8af78129d34747b943db69c481c902
Ilkka Kuosmanen <ilkka.kuosmanen@arction.com>
2019-08-05 13:40:58 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,86 @@
+# Span chart
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+- Open the project folder in a terminal:
+
+        npm install              # fetches dependencies
+        npm start                # builds an application and starts the development server
+
+- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+
+### Description 
+
+*Also known as a Range Bar, Column Graph, Floating Bar Graph, Difference Graph, High-Low Graph*
+
+This example shows creation of a Span Chart made on user side by utilizing RectangleSeries. Span Charts display categorized value ranges - focusing only on the extreme values. Each category can have multiple 'spans'.
+
+Here's the creation of a Span Chart using a pre-defined interface.
+
+```javascript
+// Create Chart.
+const chart = spanChart()
+// Add categories.
+const tools = chart.addCategory('Tools')
+const fools = chart.addCategory('Fools')
+// Add spans as min-max.
+tools.addSpan(100, 150)
+fools.addSpan(50, 75)
+
+// Adding a span returns the actual Figure that represents it, which can be styled.
+fools.addSpan(200, 250)
+    .setFillStyle(new SolidFill().setColor(prettyColor))
+    .setStrokeStyle(emptyLine)
+```
+
+The actual Span Chart logic just serves to provide a starting point for users to create their own API based on their needs and preferences. The functionality can be easily extended to construct ***Gantt Chart***.
+
+### API links
+
+* XY cartesian chart: [ChartXY][]
+* Rectangle series: [RectangleSeries][]
+* Axis: [Axis][]
+* Empty TickStyle: [emptyTick][]
+* Solid FillStyle: [SolidFill][]
+* Empty FillStyle: [emptyFill][]
+* Solid LineStyle: [SolidLine][]
+* Empty LineStyle: [emptyLine][]
+* RGBA color factory: [ColorRGBA][]
+* Color palettes: [ColorPalettes][]
+
+
+### Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [Arction][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+
+© Arction Ltd 2009-2019. All rights reserved.
+
+[0]: https://github.com/Arction/
+[1]: https://www.arction.com/lightningchart-js-api-documentation/
+[2]: https://www.arction.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://www.arction.com/support-services/
+
+[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/axis.html
+[ChartXY]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/chartxy.html
+[ColorPalettes]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#colorpalettes
+[ColorRGBA]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#colorrgba
+[emptyFill]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#emptyfill
+[emptyLine]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#emptyline
+[emptyTick]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#emptytick
+[RectangleSeries]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/rectangleseries.html
+[SolidFill]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/solidfill.html
+[SolidLine]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/solidline.html
---
+++ src/index.js
@@ -0,0 +1,191 @@
+/**
+ * LightningChartJS example that showcases creation of a Span-chart.
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart,
+    SolidFill,
+    ColorRGBA,
+    emptyLine,
+    emptyFill,
+    emptyTick,
+    ColorPalettes,
+    SolidLine,
+    UIOrigins,
+    UIElementBuilders,
+    UILayoutBuilders,
+    UIDraggingModes
+} = lcjs
+
+const lc = lightningChart()
+
+// Titles for span
+const titles = [
+    'Certificate exams',
+    'Interview',
+    'Trainee training program',
+    'Department competition training sessions',
+    'Internet security training',
+    'Scrum meeting',
+    'Development meeting',
+    'First Aid training',
+    'Conquering the silence - How to improve your marketing'
+]
+
+// Define an interface for creating span charts.
+let spanChart
+// User side SpanChart logic.
+{
+    spanChart = () => {
+        // Create a XY-Chart and add a RectSeries to it for rendering rectangles.
+        const chart = lc.ChartXY()
+            .setTitle('Conference Room Reservations')
+            .setMouseInteractions(false)
+            // Disable default AutoCursor
+            .setAutoCursorMode(0)
+            .setPadding({ right: '2' })
+        const rectangles = chart.addRectangleSeries()
+
+        const axisX = chart.getDefaultAxisX()
+            .setMouseInteractions(false)
+            // Hide default ticks, instead rely on CustomTicks.
+            .setTickStyle(emptyTick)
+
+        const axisY = chart.getDefaultAxisY()
+            .setMouseInteractions(false)
+            .setTitle('Conference Room')
+            // Hide default ticks, instead rely on CustomTicks.
+            .setTickStyle(emptyTick)
+
+        let y = 0
+        for (let i = 8; i <= 20; i++) {
+            const label = i > 12 ? i - 12 + 'PM' : i + 'AM'
+            axisX.addCustomTick()
+                .setValue(i)
+                .setTickLength(4)
+                .setGridStrokeLength(0)
+                .setTextFormatter(_ => label)
+                .setMarker(marker => marker
+                    .setBackground(background => background
+                        .setFillStyle(emptyFill)
+                        .setStrokeStyle(emptyLine)
+                    )
+                )
+        }
+
+
+        const figureHeight = 10
+        const figureThickness = 10
+        const figureGap = figureThickness * .5
+        const fitAxes = () => {
+            // Custom fitting for some additional margins
+            axisY.setInterval(y, figureHeight * .5)
+        }
+
+        let customYRange = figureHeight + figureGap * 1.6
+        const addCategory = (category) => {
+            const categoryY = y
+
+            const addSpan = (i, min, max, index) => {
+                // Add rect
+                const rectDimensions = {
+                    x: min,
+                    y: categoryY - figureHeight,
+                    width: max - min,
+                    height: figureHeight
+                }
+                //Add element for span labels
+                const spanText = chart.addUIElement(
+                    UILayoutBuilders.Row,
+                    { x: axisX.scale, y: axisY.scale }
+                )
+                    .setOrigin(UIOrigins.Center)
+                    .setDraggingMode(UIDraggingModes.notDraggable)
+                    .setPosition({
+                        x: (min + max) / 2,
+                        y: rectDimensions.y + 5,
+                    })
+                    .setBackground(background => background
+                        .setStrokeStyle(emptyLine)
+                    )
+
+                spanText.addElement(
+                    UIElementBuilders.TextBox
+                        .addStyler(
+                            textBox =>
+                                textBox.setFont(fontSettings => fontSettings.setSize(10)).setText(titles[index])
+                                    .setTextFillStyle(new SolidFill().setColor(ColorRGBA(255, 255, 255)))
+
+                        )
+
+                )
+                if (index != i) {
+                    customYRange = customYRange + figureHeight + 1
+
+                }
+                fitAxes()
+                // Return figure
+                return rectangles.add(rectDimensions)
+            }
+
+            // Add custom tick for category
+            axisY.addCustomTick()
+                .setValue(y - figureHeight * 0.5)
+                .setGridStrokeLength(0)
+                .setTextFormatter(_ => category)
+                .setMarker(marker => marker
+                    .setPadding(4)
+                    .setBackground(background => background
+                        .setFillStyle(emptyFill)
+                        .setStrokeStyle(emptyLine)
+                    )
+                )
+            y -= figureHeight * 1.5
+
+            fitAxes()
+            // Return interface for category.
+            return {
+                addSpan
+            }
+        }
+        // Return interface for span chart.
+        return {
+            addCategory
+        }
+    }
+}
+
+// Use the interface for example.
+const chart = spanChart()
+const categories = ['5 chair room', '5 chair room', '5 chair room', '10 chair room', '10 chair room', '20 chair room', 'Conference Hall'].map((name) => chart.addCategory(name))
+const colorPalette = ColorPalettes.flatUI(categories.length)
+const fillStyles = categories.map((_, i) => new SolidFill({ color: colorPalette(i) }))
+const strokeStyle = new SolidLine({
+    fillStyle: new SolidFill({ color: ColorRGBA(0, 0, 0) }),
+    thickness: 2
+})
+const spans = [
+    [[10, 13], [16, 18]],
+    [[8, 17]],
+    [[12, 20]],
+    [[9, 17]],
+    [[10, 12], [15, 19]],
+    [[11, 16]],
+    [[9, 18]]
+]
+
+let index = 0;
+spans.forEach((values, i) => {
+    values.forEach((value, j) => {
+        categories[i].addSpan(i, value[0], value[1], index)
+            .setFillStyle(fillStyles[i])
+            .setStrokeStyle(strokeStyle)
+        index = index + 1
+    }
+    )
+})
+
+
